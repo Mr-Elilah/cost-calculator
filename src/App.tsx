8d67f053cf1df6cfb1482008baa100603b59fb2b
Mr.Elijah <beztalannyj.ilia@gmail.com>
2026-01-31 20:09:45 +0100

    init: vite + react + typescript + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900 p-4">
+      <h1 className="text-xl font-semibold">Калькулятор себестоимости</h1>
+    </div>
+  );
+}
+
+export default App;
